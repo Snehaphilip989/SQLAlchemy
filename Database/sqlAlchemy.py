fa78e68821ae6c711e7effec11b029f27b98db94
Sneha Philip <snehaphilip989@gmail.com>
2019-11-28 18:21:33 -0500

    Feature: Updated pyhton file with Customer and Item Class

--- Database/sqlAlchemy.py
+++ Database/sqlAlchemy.py
@@ -9,3 +9,22 @@ engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
 Base = declarative_base()
 
 
+class Customer(Base):
+    __tablename__ = 'Customer'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column(String)
+    email = Column(String)
+    address = Column(String)
+    town = Column(String)
+
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+    quantity = Column(Integer(), nullable=False)
